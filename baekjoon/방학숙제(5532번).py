b006022a5e49f92a734ea249359a98e4e5ce0c9d
minsuhaha <haminsu5@gmail.com>
2023-07-19 22:53:26 +0900

    10988.팰린드롬

--- baekjoon/방학숙제(5532번).py
+++ baekjoon/방학숙제(5532번).py
@@ -4,8 +4,14 @@ B = int(input()) # 총 풀어야 되는 수학 페이지
 C = int(input()) # 상근이 자식이 하루에 풀 수 있는 국어 페이지
 D = int(input()) # 상근이 자식이 하루에 풀 수 있는 수학 페이지
 
+# 첫번째 방법
 while (A > 0 or B > 0):
     A -= C
     B -= D
     L -= 1
 print(L)
+
+# 두번째 방법
+result1 = A//C if A%C == 0 else A//C + 1 
+result2 = B//D if B%D == 0 else B//D + 1
+print(L - max(result1, result2))
---
+++ baekjoon/팰린드롬(10988번).py
@@ -0,0 +1,28 @@
+word = input()
+
+# 1. 재귀 함수를 사용하지 않고 문자열 뒤집기 방법을 통해서 팰린드롬 해결
+# word의 길이가 짝수일 경우
+if len(word) % 2 == 0:
+    if word[:(len(word)//2)] == word[(len(word)-1):(len(word)//2-1) : -1]:
+        print(1)
+    else:
+        print(0)
+# word의 길이가 홀수일 경우
+else:
+    if word[:(len(word)//2)] == word[(len(word)-1):(len(word)//2) : -1]:
+        print(1)
+    else:
+        print(0)
+
+# 2. 재귀함수를 사용하여 팰린드롬 해결
+def palindrome(word):
+    if len(word) <= 1:
+        return 1
+    # word의 첫글자와 word의 마지막 글자가 다를 때 팰린드롬이 아님
+    elif word[0] != word[-1]:
+        return 0
+    # 맨 앞 맨 뒤 글자가 같기 때문에 잘라서 다시 재귀함수
+    return palindrome(word[1:-1])
+
+result = palindrome(word)
+print(result)
