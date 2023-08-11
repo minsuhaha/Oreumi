36073674992806fa6708e0d44afecb5ddd25f7d3
minsuhaha <haminsu5@gmail.com>
2023-08-11 10:03:32 +0900

    타노스(20310번)

--- baekjoon/타노스(20310번).py
+++ baekjoon/타노스(20310번).py
@@ -1,13 +1,15 @@
 s = input()
 
+# s 문자열에 '1'의 빈도수의 절반만 앞에서 부터 제거하기
 new_s = s.replace('1','',s.count('1')//2)
 
 # 뒤집기
 new_s = new_s[::-1]
 
+# s 문자열에 '0'의 빈도수의 절반만 앞에서 부터 제거하기
 new_s = new_s.replace('0', '', s.count('0')//2)
 
-# 다시 뒤집기
+# 원래대로 다시 뒤집기
 new_s = new_s[::-1]
 
 print(new_s)
