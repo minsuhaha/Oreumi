48ceb0ba7f61f6d1fc27316b408220d09e18c4ad
minsuhaha <haminsu5@gmail.com>
2023-08-31 22:46:14 +0900

    로프(2217번)

---
+++ baekjoon/로프 (2217번).py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+n = int(input())
+
+weight = [int(input().rstrip()) for _ in range(n)]
+weight.sort(reverse=True)
+
+result = []
+cnt = 1
+for w in weight:
+    result.append(w*cnt)
+    cnt += 1
+
+print(result)
